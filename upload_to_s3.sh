35f6d273f4c8210d8c72530e3a5275f67b1acdac
Cayke Prudente <cayke10@gmail.com>
2019-05-22 19:26:53 -0300

    - url update

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -2,5 +2,6 @@
 
 # must exec npm install -g s3-deploy before
 
-s3-deploy './build/index.html' --cwd './build/' --region sa-east-1 --bucket ib-sites/docs.instabuy.com.br --gzip --cache 120 --deleteRemoved
-s3-deploy './build/**' --cwd './build/' --region sa-east-1 --bucket ib-sites/docs.instabuy.com.br --gzip html,js,css --cache 31536000 --deleteRemoved
+s3-deploy './build/index.html' --cwd './build/' --region sa-east-1 --bucket docs.instabuy.com.br --gzip --cache 120 --deleteRemoved
+s3-deploy './build/linear.html' --cwd './build/' --region sa-east-1 --bucket docs.instabuy.com.br --gzip --cache 120 --deleteRemoved
+s3-deploy './build/**' --cwd './build/' --region sa-east-1 --bucket docs.instabuy.com.br --gzip html,js,css --cache 31536000 --deleteRemoved
