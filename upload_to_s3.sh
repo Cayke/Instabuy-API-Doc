a4525fc6f8f5fba146fffff0cd5e436cb38ff716
Cayke Prudente <cayke10@gmail.com>
2020-05-13 19:44:57 -0300

    - minor fix

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -2,7 +2,9 @@
 
 # must exec npm install -g s3-deploy before
 
-s3-deploy './build/index.html' --cwd './build/' --region sa-east-1 --bucket docs.instabuy.com.br --gzip --cache 120 --deleteRemoved
-s3-deploy './build/linear.html' --cwd './build/' --region sa-east-1 --bucket docs.instabuy.com.br --gzip --cache 120 --deleteRemoved
-s3-deploy './build/dmcard.html' --cwd './build/' --region sa-east-1 --bucket docs.instabuy.com.br --gzip --cache 120 --deleteRemoved
-s3-deploy './build/**' --cwd './build/' --region sa-east-1 --bucket docs.instabuy.com.br --gzip html,js,css --cache 31536000 --deleteRemoved
+s3-deploy './build/index.html' --cwd './build/' --bucket websites.instabuy.com.br/docs.instabuy.com.br --gzip --cache 120 --deleteRemoved
+s3-deploy './build/linear.html' --cwd './build/' --bucket websites.instabuy.com.br/docs.instabuy.com.br --gzip --cache 120 --deleteRemoved
+s3-deploy './build/dmcard.html' --cwd './build/' --bucket websites.instabuy.com.br/docs.instabuy.com.br --gzip --cache 120 --deleteRemoved
+s3-deploy './build/admin.html' --cwd './build/' --bucket websites.instabuy.com.br/docs.instabuy.com.br --gzip --cache 120 --deleteRemoved
+s3-deploy './build/tetra.html' --cwd './build/' --bucket websites.instabuy.com.br/docs.instabuy.com.br --gzip --cache 120 --deleteRemoved
+s3-deploy './build/**' --cwd './build/' --bucket websites.instabuy.com.br/docs.instabuy.com.br --gzip html,js,css --cache 31536000 --deleteRemoved
